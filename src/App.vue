342637deda72addc8d048d3ef7d571a905fd8473
sajib <mhsajib44@gmail.com>
2021-11-06 21:20:42 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+      <a href="#" class="navbar-brand">bezKoder</a>
+      <div class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a href="/home" class="nav-link">
+            <font-awesome-icon icon="home" /> Home
+          </a>
+        </li>
+        <li class="nav-item" v-if="showAdminBoard">
+          <a href="/admin" class="nav-link">Admin Board</a>
+        </li>
+        <li class="nav-item" v-if="showModeratorBoard">
+          <a href="/mod" class="nav-link">Moderator Board</a>
+        </li>
+        <li class="nav-item">
+          <a href="/user" class="nav-link" v-if="currentUser">User</a>
+        </li>
+      </div>
+
+      <div class="navbar-nav ml-auto" v-if="!currentUser">
+        <li class="nav-item">
+          <a href="/register" class="nav-link">
+            <font-awesome-icon icon="user-plus" /> Sign Up
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="/login" class="nav-link">
+            <font-awesome-icon icon="sign-in-alt" /> Login
+          </a>
+        </li>
+      </div>
+
+      <div class="navbar-nav ml-auto" v-if="currentUser">
+        <li class="nav-item">
+          <a href="/profile" class="nav-link">
+            <font-awesome-icon icon="user" />
+            {{currentUser.username}}
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href class="nav-link" @click="logOut">
+            <font-awesome-icon icon="sign-out-alt" /> LogOut
+          </a>
+        </li>
+      </div>
+    </nav>
+
+    <div class="container">
+      <router-view />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  computed: {
+    currentUser() {
+      return this.$store.state.auth.user;
+    },
+    showAdminBoard() {
+      if (this.currentUser) {
+        // return this.currentUser.roles.includes('ROLE_ADMIN');
+      }
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+      return false;
+    },
+    showModeratorBoard() {
+      if (this.currentUser) {
+        // return this.currentUser.roles.includes('ROLE_MODERATOR');
+      }
+
+      return false;
+    }
+  },
+  methods: {
+    logOut() {
+      this.$store.dispatch('auth/logout');
+      this.$router.push('/login');
+    }
+  }
+};
+</script>
